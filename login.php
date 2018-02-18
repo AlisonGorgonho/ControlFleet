2dac8287c4854f486a87f9af166e23d1908fb070
Alison Gorgonho <alisongorgonho@gmail.com>
2018-02-18 01:53:51 -0300

    Incluindo página de login

---
+++ login.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" type="text/css" href="webroot/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="webroot/css/login.css">
+
+    <title>Login ControlFleet</title>
+
+  </head>
+
+  <body class="text-center">
+    <form class="form-signin">
+      <img class="mb-4" src="webroot/img/icone.png" alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal">Fazer Login</h1>
+      <label for="inputEmail" class="sr-only">Email</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+      <label for="inputPassword" class="sr-only">Senha</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Relembrar
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2018-2018</p>
+    </form>
+  </body>
+</html>
